05b2a7c15a55a80469452e0596d86588ecfb8b93
Pekka Luostarinen <pekka.luostarinen@gmail.com>
2023-01-11 10:57:08 +0200

    add pwsh

--- scripts/install-apps.ps1
+++ scripts/install-apps.ps1
@@ -2,6 +2,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 #Install required apps
 choco install git.install azure-cli --force -y
-#set git bin folder to path
-$Env:PATH += ";C:\Program Files\Git\bin"
+choco install pwsh --force -y
 
